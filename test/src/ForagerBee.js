c6c686825f064c45cff7f3649f5648a9a1fb9cc5
Hack Reactor Student <student@hackreactor.com>
2017-07-10 16:12:23 -0700

    Completed Full Pass

---
+++ test/src/ForagerBee.js
@@ -0,0 +1,13 @@
+var ForagerBee = function() {
+  Bee.call(this);
+  this.job = 'find pollen';
+  this.age = 10;
+  this.canFly = true;
+  this.treasureChest = [];
+};
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
